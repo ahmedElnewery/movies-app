85acfc61b481bad2cf582f8c4df71bb5615c1d3c
ahmedElnewery <ahmedm.elnewery@gmail.com>
2023-08-10 12:14:06 +0300

    extract movie card component

---
+++ src/modules/movies/components/MovieCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import IconBookmark from "@/components/icons/IconBookmark.vue";
+import type { IMovie } from "../@types/movies";
+defineProps<{
+  movie: IMovie;
+}>();
+</script>
+<template>
+  <div class="relative group rounded-md overflow-hidden">
+    <div class="w-full">
+      <img
+        class="object-contain w-full"
+        :src="movie.image"
+        :alt="movie.title"
+      />
+    </div>
+    <RouterLink
+      :to="`/movies/${movie.id}`"
+      class="absolute inset-0 z-20"
+    ></RouterLink>
+    <div
+      class="absolute z-30 left-0 right-0 top-0 flex h-1/4 w-full flex-col justify-end overlay-top px-4 pb-4 opacity-100 transition-opacity duration-300"
+    >
+      <button class="absolute top-6 right-6 text-yellow">
+        <IconBookmark />
+      </button>
+    </div>
+    <div
+      class="absolute left-0 right-0 bottom-0 z-10 flex h-1/2 w-full flex-col justify-end overlay-bottom px-4 pb-3 opacity-0 transition-opacity duration-300 group-hover:opacity-90"
+    >
+      <p class="text-white truncate">{{ movie.fullTitle }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.overlay-top {
+  background: linear-gradient(to top, transparent 0%, black 75%);
+}
+.overlay-bottom {
+  background: linear-gradient(to bottom, transparent 0%, black 75%);
+}
+</style>
--- src/modules/movies/view/Index.vue
+++ src/modules/movies/view/Index.vue
@@ -3,7 +3,7 @@ import { ref, type Ref } from "vue";
 import type { IMovie } from "./../@types/movies";
 import MoviesService from "./../services/movies.service";
 import BaseContainer from "@/components/partials/base/BaseContainer.vue";
-import IconBookmark from "@/components/icons/IconBookmark.vue";
+import MovieCard from "../components/MovieCard.vue";
 
 //data
 const loading: Ref<boolean> = ref(false);
@@ -35,46 +35,15 @@ async function fetchMovies() {
         <div
           class="grid xl:grid-cols-5 lg:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-8"
         >
-          <div
-            class="relative group rounded-md overflow-hidden"
+          <MovieCard
             v-for="movie in moviesList"
+            :movie="movie"
             :key="movie.id"
-          >
-            <div class="w-full">
-              <img
-                class="object-contain w-full"
-                :src="movie.image"
-                :alt="movie.title"
-              />
-            </div>
-            <RouterLink
-              :to="`/movies/${movie.id}`"
-              class="absolute inset-0 z-20"
-            ></RouterLink>
-            <div
-              class="absolute z-30 left-0 right-0 top-0 flex h-1/4 w-full flex-col justify-end overlay-top px-4 pb-4 opacity-100 transition-opacity duration-300"
-            >
-              <button class="absolute top-6 right-6 text-yellow fill-yellow">
-                <IconBookmark />
-              </button>
-            </div>
-            <div
-              class="absolute left-0 right-0 bottom-0 z-10 flex h-1/2 w-full flex-col justify-end overlay-bottom px-4 pb-3 opacity-0 transition-opacity duration-300 group-hover:opacity-90"
-            >
-              <p class="text-white truncate">{{ movie.fullTitle }}</p>
-            </div>
-          </div>
+          />
         </div>
       </BaseContainer>
     </div>
   </main>
 </template>
 
-<style scoped>
-.overlay-top {
-  background: linear-gradient(to top, transparent 0%, black 75%);
-}
-.overlay-bottom {
-  background: linear-gradient(to bottom, transparent 0%, black 75%);
-}
-</style>
+<style scoped></style>
