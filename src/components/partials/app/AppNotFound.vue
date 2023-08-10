3cff414c242cff1e2f329b44ea1a2fef66ec34bb
ahmedElnewery <ahmedm.elnewery@gmail.com>
2023-08-10 22:23:49 +0300

    adding not found page

---
+++ src/components/partials/app/AppNotFound.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import BaseContainer from "../base/BaseContainer.vue";
+import AppPage from "./AppPage.vue";
+</script>
+<template>
+  <AppPage class="bg-muted-100">
+    <BaseContainer>
+      <div class="perfect-center pt-12">
+        <div class="sm:max-w-xs lg:max-w-lg">
+          <img
+            class="w-full"
+            src="@/assets/img/not-found.svg"
+            alt="not found"
+          />
+        </div>
+      </div>
+    </BaseContainer>
+  </AppPage>
+</template>
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,21 +2,27 @@ import { createRouter, createWebHistory } from "vue-router";
 import homeRoutes from "@/modules/home/home.routes";
 import moviesRoutes from "@/modules/movies/movies.routes";
 import authRoutes from "@/modules/auth/auth.routes";
-import DefaultLayout from "@/components/layouts/Default.vue";
-import AuthLayout from "@/components/layouts/Auth.vue";
+import DefaultLayout from "@/layouts/Default.vue";
+import AuthLayout from "@/layouts/Auth.vue";
+import AppNotFound from "@/components/partials/app/AppNotFound.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
       component: DefaultLayout,
-      children: [...homeRoutes, ...moviesRoutes],
+      children: [
+        ...homeRoutes,
+        ...moviesRoutes,
+        { path: "/:pathMatch(.*)*", name: "NotFound", component: AppNotFound },
+      ],
     },
     {
       path: "/auth",
       component: AuthLayout,
       children: [...authRoutes],
     },
+    ,
   ],
 });
 
